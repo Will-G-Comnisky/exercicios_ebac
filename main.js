1bf783521d6ee14b0f02838255337c3022199df9
William Gispiela Comnisky <william.gispiela@gmail.com>
2023-02-08 21:04:25 -0500

    commit segunda tentativa

--- main.js
+++ main.js
@@ -1,18 +1,36 @@
 $(document).ready(function() {
 
-    $('#input').change(function() {
-        var input = $(this).val();
-        $('ul').append('<li>' + input + ' <i class="fas fa-check"></i> <i class="fas fa-trash"></i> </li>');
-        $(this).val('');
-    });
+    $('#telefone').mask('(00) 00000-0000')
 
-    $('ul').on('click', '.fa-trash', function() {
-        $(this).parent('li').fadeOut(200);
+    $('form').validate ({
+        rules: {
+            nome: {
+                required: true
+            },
+            email: {
+                required: true,
+                email: true
+            }, 
+            telefone: {
+                required: true
+            },
+            mensagem: {
+                required: false
+            }
+        },
+        messages: {
+            nome: 'Por favor, insira o seu nome',
+            telefone: 'Por favor, insira o seu nº de telefone',
+            email: 'Por favor, insira o seu e-mail',
+        },
+        submitHandler: function(form) {
+            console.log(form)
+        },
+        invalidHandler: function(evento, validador) {
+            let camposIncorretos = validador.numberOfInvalids();
+            if (camposIncorretos) {
+                alert(`Existem ${camposIncorretos} campos incorretos`)
+            }
+        }
     })
-
-    $('ul').on('click', '.fa-check', function() {
-        $(this).parent('li').toggleClass('checked');
-    })
- 
-
-});
+})
